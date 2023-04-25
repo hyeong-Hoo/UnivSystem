17190932bbf4d9e764408327aa3202ba966cb276
준수 <@DESKTOP-LPKFVAD>
2023-04-25 17:30:37 +0900

    test

--- src/main/java/com/teamp/cau/dao/RecruitDAO.java
+++ src/main/java/com/teamp/cau/dao/RecruitDAO.java
@@ -2,19 +2,15 @@ package com.teamp.cau.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.teamp.cau.dto.RecruitDTO;
 
-public class RecruitDAO {
-	
-	@Autowired
-	private SqlSession sqlSession;
-	
-	public List<RecruitDTO> selectList() {
-		
-		return sqlSession.selectList("recruit.selectList");
-	}
+@Repository
+@Mapper
+public interface RecruitDAO {
+
+	List<RecruitDTO> selectList();
 
 }
