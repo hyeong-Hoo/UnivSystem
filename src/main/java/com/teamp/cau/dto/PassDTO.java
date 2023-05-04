6491595b58b29612a37cded8ee97cb49502facfc
wookjji <dnrworkqqn12@gmail.com>
2023-05-04 14:38:25 +0900

    payment

--- src/main/java/com/teamp/cau/dto/PassDTO.java
+++ src/main/java/com/teamp/cau/dto/PassDTO.java
@@ -7,7 +7,7 @@ public class PassDTO {
 	
 	private String KORN_FLNM, USER_BRDT, GENDER_CD, EML_ADDR, TELNO, REL_TELNO,STUD_NO,RECRT_SCHDL_CD,RECRT_YEAR,
 	        REL_CD, ZIP, ADDR, DADDR, USER_NO, CRCLM_CD, EDU_FNSH_YN, PDF,department,category,Student_ID,CRCLM,passed,SCHDL_NAME;
-	private int AGE, CRCLM_CYCL,PASS_INFO,appl_NO,avg,APPL_NUM; 
+	private int AGE, CRCLM_CYCL,PASS_INFO,appl_NO,avg,APPL_NUM,payment; 
     private byte[] PHOTO_FILE;
 
 }
