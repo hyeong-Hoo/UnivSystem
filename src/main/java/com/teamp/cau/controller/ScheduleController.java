73663b0f23d8f178ac2f42919d3c0172eb7406f1
Sjunsu <soile1595@gmail.com>
2023-05-04 21:25:37 +0900

    scheduleController 수정

--- src/main/java/com/teamp/cau/controller/ScheduleController.java
+++ src/main/java/com/teamp/cau/controller/ScheduleController.java
@@ -6,9 +6,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.teamp.cau.dto.ScheduleDTO;
+import com.teamp.cau.service.RecruitService;
 import com.teamp.cau.service.ScheduleService;
 
 @Controller
@@ -16,10 +19,15 @@ public class ScheduleController {
 	
 	@Autowired
 	private ScheduleService scheduleService;
+	@Autowired
+	private RecruitService recruitService;
 	
 	@GetMapping("/schedule")
-	public String Schedule() {
-		return "schedule";
+	public ModelAndView Schedule() {
+		ModelAndView mv = new ModelAndView("schedule");
+		
+		mv.addObject("year", recruitService.year());
+		return mv;
 	}
 	
 	@GetMapping("/sch")
@@ -27,6 +35,15 @@ public class ScheduleController {
 	public List<Map<String, Object>> schtable(){
 		List<Map<String, Object>> list = scheduleService.tableList();
 		
+		return list;
+	}
+	@GetMapping("/scheduleCheck")
+	@ResponseBody
+	public List<Map<String, Object>> scheduleCheck(@RequestParam("year") String year){
+		ScheduleDTO dto = new ScheduleDTO();
+		dto.setRECRT_YEAR(year);
+		List<Map<String, Object>> list = scheduleService.scheduleCheck(dto);
+		System.out.println(list);
 		return list;
 	}
 }
