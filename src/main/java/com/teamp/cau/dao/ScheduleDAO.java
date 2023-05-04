aab5076ce9d803011feac72067bb936188cd2a34
Sjunsu <soile1595@gmail.com>
2023-05-04 21:29:07 +0900

    scheduleDAO 수정

--- src/main/java/com/teamp/cau/dao/ScheduleDAO.java
+++ src/main/java/com/teamp/cau/dao/ScheduleDAO.java
@@ -6,10 +6,14 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import com.teamp.cau.dto.ScheduleDTO;
+
 @Repository
 @Mapper
 public interface ScheduleDAO {
 
 	List<Map<String, Object>> tableList();
+
+	List<Map<String, Object>> scheduleCheck(ScheduleDTO dto);
 
 }
