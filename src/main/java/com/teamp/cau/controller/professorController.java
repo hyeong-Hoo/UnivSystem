0851934a8ed909d13d823258ae765f12af2f3dad
jhhya424@naver.com <jhhya424@gmail.com>
2023-05-11 15:51:46 +0900

    fdsf

--- src/main/java/com/teamp/cau/controller/professorController.java
+++ src/main/java/com/teamp/cau/controller/professorController.java
@@ -20,24 +20,19 @@ import com.teamp.cau.service.professorService;
 
 @Controller
 public class professorController {
-	
+
 	@Autowired
 	private professorService prService;
+
 	
-	// 메인페이지 
-	@GetMapping("/index")
-	public String test() {
-		return "index";
-	}
-	
-	
-	// 교수정보입력 페이지 
+
+	// 교수정보입력 페이지
 	@GetMapping("/professor")
 	public String pro_write() {
 		return "professor";
 	}
-	
-	// 교수정보입력 저장 
+
+	// 교수정보입력 저장
 	@PostMapping("/professor_info")
 	public String info_save(HttpServletRequest req) {
 		String pr_id = req.getParameter("pr_id");
@@ -58,26 +53,26 @@ public class professorController {
 		infoEnter.put("pr_birth", pr_birth);
 		infoEnter.put("pr_email", pr_email);
 		System.out.println(infoEnter);
-		Integer enter = prService.prinfo_save(infoEnter);	// instr_info로
-		Integer enter2 = prService.instr_user(infoEnter);	// user_info로 
+		Integer enter = prService.prinfo_save(infoEnter); // instr_info로
+		Integer enter2 = prService.instr_user(infoEnter); // user_info로
 		return "professor";
 	}
-	
-	
-	// 교수본인정보 수정 불러오기 
+
+	// 교수본인정보 수정 불러오기
 	@GetMapping("/self_modify")
-	public String modify( ) {
+	public String modify() {
 		return "self_modify";
 	}
-	
-	// 교수본인정보 수정 
-	@PostMapping("/pr_self")
-	public String self_save(@RequestParam("pr_no") String pr_no, @RequestParam("name_kr") String name_kr, 
-			@RequestParam("name_en") String name_en,@RequestParam("roadAddrPart1") String roadAddrPart1,@RequestParam("addrDetail") String addrDetail,
-			@RequestParam("pr_telno") String pr_telno,@RequestParam("pr_email") String pr_email,@RequestParam("pr_birth") String pr_birth,
-			@RequestParam("pr_gender") String pr_gender,@RequestParam("image") MultipartFile image) {
 
-	    ConvertBinary convert = new ConvertBinary();
+	// 교수본인정보 수정
+	@PostMapping("/pr_self")
+	public String self_save(@RequestParam("pr_no") String pr_no, @RequestParam("name_kr") String name_kr,
+			@RequestParam("name_en") String name_en, @RequestParam("roadAddrPart1") String roadAddrPart1,
+			@RequestParam("addrDetail") String addrDetail, @RequestParam("pr_telno") String pr_telno,
+			@RequestParam("pr_email") String pr_email, @RequestParam("pr_birth") String pr_birth,
+			@RequestParam("pr_gender") String pr_gender, @RequestParam("image") MultipartFile image) {
+
+		ConvertBinary convert = new ConvertBinary();
 		Map<String, Object> selfmodi = new HashMap<>();
 		selfmodi.put("pr_no", pr_no);
 		selfmodi.put("name_kr", name_kr);
@@ -89,15 +84,14 @@ public class professorController {
 		selfmodi.put("pr_birth", pr_birth);
 		selfmodi.put("pr_gender", pr_gender);
 		String images;
-	    images = convert.convertBinary(image);
-	    selfmodi.put("IMG_FILE", images);
+		images = convert.convertBinary(image);
+		selfmodi.put("IMG_FILE", images);
 		System.out.println(selfmodi);
 		prService.selfSave(selfmodi);
 		return "self_modify";
 	}
-	
 
-	//교수정보 불러오기 
+	// 교수정보 불러오기
 	@GetMapping("/pr_info")
 	public ModelAndView prinfo(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("pr_info");
@@ -105,8 +99,8 @@ public class professorController {
 		mv.addObject("list", list);
 		return mv;
 	}
-	
-	//교수정보 검색 
+
+	// 교수정보 검색
 	@PostMapping("/pr_info_search")
 	public ModelAndView search(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView("pr_info");
@@ -115,55 +109,54 @@ public class professorController {
 		String code_search = request.getParameter("code_search");
 //		System.out.println(name_search);
 //		System.out.println(code_search);
-		HashMap<String,String> map=new HashMap<>();
+		HashMap<String, String> map = new HashMap<>();
 		map.put("name_search", name_search);
 		map.put("code_search", code_search);
 		List<professorDTO> list = prService.selectList(map);
 		mv.addObject("list", list);
 		return mv;
 	}
-	
-	//권한주기 
+
+	// 권한주기
 	@PostMapping("/pr_info_auth")
-	public String auth(@RequestParam("num") int num, @RequestParam HashMap<String, Object> autest ) {
+	public String auth(@RequestParam("num") int num, @RequestParam HashMap<String, Object> autest) {
 		professorDTO authdto = new professorDTO();
 		for (int i = 0; i < num; i++) {
-			int pr_no = Integer.parseInt((String)autest.get("auth_test [" + i + "][no]" ));
-			int authority = Integer.parseInt((String)autest.get("auth_test [" + i + "][auth]" ));
+			int pr_no = Integer.parseInt((String) autest.get("auth_test [" + i + "][no]"));
+			int authority = Integer.parseInt((String) autest.get("auth_test [" + i + "][auth]"));
 			authdto.setINSTR_NO(pr_no);
 			authdto.setENDST_NO(authority);
 			prService.test(authdto);
 			System.out.println(pr_no);
 		}
-		
+
 		return "redirect:/pr_info";
 	}
-	
-	
+
 	// 면접평가 불러오기
 	@GetMapping("/pr_authority")
 	public ModelAndView call_in() {
 		ModelAndView mv = new ModelAndView("pr_authority");
 		List<HashMap<String, Object>> appl_list = prService.selectList();
-		//System.out.println(appl_list);
+		// System.out.println(appl_list);
 		mv.addObject("appl_list", appl_list);
 		return mv;
 	}
-	
-	// 면접평가 및 총 점수 저장 
+
+	// 면접평가 및 총 점수 저장
 	@PostMapping("/pr_score")
 	public String score(HttpServletRequest req) {
 		String[] appl_no = req.getParameterValues("appl_no");
 		String[] interview = req.getParameterValues("inter_score");
-		List<HashMap<String,Integer>> list = new ArrayList<HashMap<String,Integer>>();
-		for(int i=0;i<appl_no.length;i++) {
-			HashMap<String,Integer> res_inner=new HashMap<String,Integer>();
-			res_inner.put("appl_no",Integer.parseInt(appl_no[i]));
-			res_inner.put("inter_score",Integer.parseInt(interview[i]));
+		List<HashMap<String, Integer>> list = new ArrayList<HashMap<String, Integer>>();
+		for (int i = 0; i < appl_no.length; i++) {
+			HashMap<String, Integer> res_inner = new HashMap<String, Integer>();
+			res_inner.put("appl_no", Integer.parseInt(appl_no[i]));
+			res_inner.put("inter_score", Integer.parseInt(interview[i]));
 			list.add(res_inner);
-		
+
 		}
-		int result_row=prService.resultpoint(list);
+		int result_row = prService.resultpoint(list);
 //		
 //		for (int i = 0; i < interview.length; i++) {
 //			professorDTO dto = new professorDTO();
@@ -180,25 +173,22 @@ public class professorController {
 //			}
 //		}
 //		
-		//for (professorDTO d : res) {
-		//	System.out.println(d.getAppl_NO() + ":::" + d.getInterview());
-		//}
-		
-		
+		// for (professorDTO d : res) {
+		// System.out.println(d.getAppl_NO() + ":::" + d.getInterview());
+		// }
+
 		professorDTO dto = new professorDTO();
 		System.out.println(req.getParameterValues("inter_score"));
 		for (int i = 0; i < appl_no.length; i++) {
-			
-			//prService.scoreSave(dto);
+
+			// prService.scoreSave(dto);
 		}
-		
-		
-		
+
 //		scoreSave.put("inter_score", interview);
 //		scoreSave.put("totalscore", total);
 //		System.out.println(Arrays.toString());
 //		Integer interviewScore = prService.resultscore("resultScore", scoreSave);
 		return "redirect:/pr_authority";
 	}
-	
+
 }
